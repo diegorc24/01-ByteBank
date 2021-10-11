9a60ac1fc180663432365dd6ae8d8cec62e126cb
Diego Rodrigues Cardoso <diego.rodrigues@timesharesolucoes.com.br>
2021-10-11 10:32:21 -0300

    Adicionar arquivos de projeto.

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabrielaCosta.titular = "Gabriela";
+            contaDaGabrielaCosta.agencia = 863;
+            contaDaGabrielaCosta.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDaGabriela == contaDaGabrielaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualde de tipo de valor: " + (idade == idadeMaisUmaVez));
+
+            contaDaGabriela = contaDaGabrielaCosta;
+            Console.WriteLine(contaDaGabriela == contaDaGabrielaCosta);
+
+            contaDaGabriela.saldo = 300;
+            Console.WriteLine(contaDaGabriela.saldo);
+            Console.WriteLine(contaDaGabrielaCosta.saldo);
+
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("primeira conta tem " + primeiraContaCorrente.saldo);
+            Console.WriteLine("segunda conta tem " + segundaContaCorrente.saldo);
+
+            Console.ReadLine();
+            Console.ReadLine();
+
+        }
+    }
+}
